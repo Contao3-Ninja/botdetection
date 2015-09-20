f0392fd9258161ec23990fef7d6e4bdf952a2f6f
Glen Langer <contao.modul@glen-langer.de>
2015-09-20 23:34:12 +0200

    Update languages files

--- src/languages/de/modules.php
+++ src/languages/de/modules.php
@@ -8,14 +8,14 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-13T19:15:05+02:00
+ * last-updated: 2015-09-20T22:41:42+02:00
  */
 
 $GLOBALS['TL_LANG']['FMD']['botdetection1']['0']         = 'Bot Detection Demo 1';
 $GLOBALS['TL_LANG']['FMD']['botdetection1']['1']         = 'Demo 1 für Bot Detection Verwendung über Import Methode.';
 $GLOBALS['TL_LANG']['FMD']['botdetection2']['0']         = 'Bot Detection Demo 2';
 $GLOBALS['TL_LANG']['FMD']['botdetection2']['1']         = 'Demo 2 für Bot Detection (Formular).';
-$GLOBALS['TL_LANG']['FMD']['browscapcachegenerate']['0'] = 'Bot Detection Browscap Cache Generator';
+$GLOBALS['TL_LANG']['FMD']['browscapcachegenerate']['0'] = 'Nur für Admins. Browscap Cache Generator';
 $GLOBALS['TL_LANG']['FMD']['browscapcachegenerate']['1'] = 'Nur für Admins. Browscap Cache Generator, Laufzeit ~90s.';
 $GLOBALS['TL_LANG']['MOD']['botdetection']['0']          = 'Bot Detection';
 $GLOBALS['TL_LANG']['MOD']['botdetection']['1']          = 'Hilfsklasse zur Erkennung von Suchmaschinen, Bots, Spider, Crawler.';
