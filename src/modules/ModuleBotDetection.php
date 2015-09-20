cc17ebedeb42bca0f64c3e5c5f766e95d471f82b
Glen Langer <contao.modul@glen-langer.de>
2015-09-21 01:37:40 +0200

    Add Deprecated Methods

--- src/modules/ModuleBotDetection.php
+++ src/modules/ModuleBotDetection.php
@@ -89,7 +89,11 @@ class ModuleBotDetection extends \System //\Frontend //TODO reicht hier nicht \S
      */
     public function checkBotAllTests($UserAgent = false)
     {
-        if ( \BugBuster\BotDetection\CheckBotAgentSimple::checkAgent( \Environment::get('httpUserAgent') ) === true ) //(BotsRough, BotsFine)
+        if (false === $UserAgent) 
+        {
+        	$UserAgent = \Environment::get('httpUserAgent');
+        }
+        if ( \BugBuster\BotDetection\CheckBotAgentSimple::checkAgent( $UserAgent ) === true ) //(BotsRough, BotsFine)
         {
             return true;
         }
@@ -109,13 +113,90 @@ class ModuleBotDetection extends \System //\Frontend //TODO reicht hier nicht \S
         else
         {
             //CheckBotAgentExtended (Browscap + eigene Liste)
-            return \BugBuster\BotDetection\CheckBotAgentExtended::checkAgent( \Environment::get('httpUserAgent') );
+            return \BugBuster\BotDetection\CheckBotAgentExtended::checkAgent( $UserAgent );
         }
     }
     
+    /////////////// Deprecated Methods ///////////////
+
+    /**
+     * Spider Bot Agent Check
+     *
+     * @param string   UserAgent, optional for tests
+     * @return boolean true when bot found
+     * @deprecated Use the CheckBotAgentSimple class instead or the method checkBotAllTests
+     */
+    public function BD_CheckBotAgent($UserAgent = false)
+    {
+        // Check if user agent present
+        if ($UserAgent === false)
+        {
+            $UserAgent = trim(\Environment::get('httpUserAgent'));
+        }
+        return \BugBuster\BotDetection\CheckBotAgentSimple::checkAgent( $UserAgent );
+    }
     
+    /**
+     * Spider Bot IP Check
+     *
+     * @param string   User IP, optional for tests
+     * @return boolean true when bot found over IP
+     * @deprecated Use the CheckBotIp class instead
+     */
+    public function BD_CheckBotIP($UserIP = false)
+    {
+        // Check if IP present
+        if ($UserIP === false)
+        {
+            if (strpos(\Environment::get('remoteAddr'), ',') !== false) //first IP
+            {
+				$UserIP = trim(substr(\Environment::get('remoteAddr'), 0, strpos(\Environment::get('remoteAddr'), ',')));
+            }
+            else
+            {
+				$UserIP = trim(\Environment::get('remoteAddr'));
+            }
+        }
+        return \BugBuster\BotDetection\CheckBotIp::checkIP( $UserIP );
+    }
     
+    /**
+     * Spider Bot Agent Check Advanced
+     *
+     * @param string	UserAgent, optional for tests
+     * @return bool	    false (not bot) or true (bot), in old version the short Bot-Agentname
+     * @deprecated Use the CheckBotAgentExtended class instead or the method checkBotAllTests
+     */
+    public function BD_CheckBotAgentAdvanced($UserAgent = false)
+    {
+        if ($UserAgent === false)
+        {
+                $UserAgent = trim(\Environment::get('httpUserAgent'));
+        }
+        return \BugBuster\BotDetection\CheckBotAgentExtended::checkAgent( $UserAgent );
+    }
     
+    /**
+     * CheckBotReferrer
+     * 
+     * @param string $Referrer
+     * @deprecated Use the CheckBotReferrer class instead or the method checkBotAllTests
+     */
+    public function BD_CheckBotReferrer($Referrer = false)
+    {
+        return \BugBuster\BotDetection\CheckBotReferrer::checkReferrer($Referrer, TL_ROOT . self::BOT_REFERRER_LIST);
+    }
     
-    
+    /**
+     * Spider Bot Agent/Advanced/Referrer/IP Check
+     *
+     * @param string   UserAgent, optional for tests
+     * @return boolean true when bot found
+     * @access public
+     * @deprecated Use the method checkBotAllTests
+     */
+    public function BD_CheckBotAllTests($UserAgent = false)
+    {
+        return $this->checkBotAllTests($UserAgent);
+    }
 }
