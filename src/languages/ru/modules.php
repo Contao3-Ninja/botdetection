d2dd8da66732af5ff0f964742d875f7ea393dc69
Glen Langer <contao.modul@glen-langer.de>
2015-10-10 22:50:13 +0200

    Fixed #125, Update translated language files (ru)

--- src/languages/ru/modules.php
+++ src/languages/ru/modules.php
@@ -8,14 +8,15 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-06-28T19:04:02+02:00
+ * last-updated: 2015-07-13T19:15:05+02:00
  */
 
-
-$GLOBALS['TL_LANG']['FMD']['botdetection1']['0'] = 'Bot Detection демо 1';
-$GLOBALS['TL_LANG']['FMD']['botdetection1']['1'] = 'Демо 1 Bot Detection';
-$GLOBALS['TL_LANG']['FMD']['botdetection2']['0'] = 'Bot Detection демо 2';
-$GLOBALS['TL_LANG']['FMD']['botdetection2']['1'] = 'Демо 2 Bot Detection (Форма).';
-$GLOBALS['TL_LANG']['MOD']['botdetection']['0']  = 'Bot Detection';
-$GLOBALS['TL_LANG']['MOD']['botdetection']['1']  = 'Вспомогательный класс для обнаружения поисковых систем, ботов, пауков, сканеров.';
+$GLOBALS['TL_LANG']['FMD']['botdetection1']['0']         = 'Bot Detection демо 1';
+$GLOBALS['TL_LANG']['FMD']['botdetection1']['1']         = 'Демо 1 Bot Detection';
+$GLOBALS['TL_LANG']['FMD']['botdetection2']['0']         = 'Bot Detection демо 2';
+$GLOBALS['TL_LANG']['FMD']['botdetection2']['1']         = 'Демо 2 Bot Detection (Форма).';
+$GLOBALS['TL_LANG']['FMD']['browscapcachegenerate']['0'] = 'Только для администраторов. Browscap Cache Generator';
+$GLOBALS['TL_LANG']['FMD']['browscapcachegenerate']['1'] = 'Только для администраторов. Browscap Cache Generator, время выполнения ~90 сек.';
+$GLOBALS['TL_LANG']['MOD']['botdetection']['0']          = 'Bot Detection';
+$GLOBALS['TL_LANG']['MOD']['botdetection']['1']          = 'Вспомогательный класс для обнаружения поисковых систем, ботов, пауков, сканеров.';
 
