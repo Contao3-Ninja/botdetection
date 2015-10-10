e2c20ea490aece1ebb1997710da587eb6990bb54
Glen Langer <contao.modul@glen-langer.de>
2015-10-10 21:22:57 +0200

    Fixed #124, add ide_compat.php

---
+++ src/config/ide_compat.php
@@ -0,0 +1,17 @@
+<?php
+
+// This file is not used in Contao. Its only purpose is to make PHP IDEs like
+// Eclipse, Zend Studio or PHPStorm realize the class origins, since the dynamic
+// class aliasing we are using is a bit too complex for them to understand.
+namespace BotDetection {
+    class ModuleBotDetection extends \BugBuster\BotDetection\ModuleBotDetection {}
+    class ModuleFrontendDemo1 extends \BugBuster\BotDetection\ModuleFrontendDemo1 {}
+    class ModuleFrontendDemo2 extends \BugBuster\BotDetection\ModuleFrontendDemo2 {}
+    class ModuleBrowscapCache extends \BugBuster\BotDetection\ModuleBrowscapCache {}
+    class ModuleBotDetectionTest extends \BugBuster\BotDetection\ModuleBotDetectionTest {}
+    class CheckBotAgentExtended extends \BugBuster\BotDetection\CheckBotAgentExtended {}
+    class CheckBotAgentSimple extends \BugBuster\BotDetection\CheckBotAgentSimple {}
+    class BrowscapCache extends \BugBuster\BotDetection\BrowscapCache {}
+    class CheckBotReferrer extends \BugBuster\BotDetection\CheckBotReferrer {}
+    class CheckBotIp extends \BugBuster\BotDetection\CheckBotIp {}
+}
