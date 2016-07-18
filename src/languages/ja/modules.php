b9f42f08a15112e5488b162689253d7980bd625f
Glen Langer <contao.modul@glen-langer.de>
2016-07-19 00:45:22 +0200

    Update translated language files (ja)

--- src/languages/ja/modules.php
+++ src/languages/ja/modules.php
@@ -8,14 +8,15 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-09-20T22:41:41+02:00
+ * last-updated: 2015-09-20T22:41:42+02:00
  */
 
-
-$GLOBALS['TL_LANG']['FMD']['botdetection1']['0'] = 'ボット検出のデモ1';
-$GLOBALS['TL_LANG']['FMD']['botdetection1']['1'] = 'ボット検出のimportメソッドを使用したデモ1。';
-$GLOBALS['TL_LANG']['FMD']['botdetection2']['0'] = 'ボット検出のデモ2';
-$GLOBALS['TL_LANG']['FMD']['botdetection2']['1'] = 'ボット検出のデモ2(フォーム)。';
-$GLOBALS['TL_LANG']['MOD']['botdetection']['0']  = 'ボット検出';
-$GLOBALS['TL_LANG']['MOD']['botdetection']['1']  = '検索エンジン、ボット、スパイダー、クローラーを検出するヘルパークラスです。';
+$GLOBALS['TL_LANG']['FMD']['botdetection1']['0']         = 'ボット検出のデモ1';
+$GLOBALS['TL_LANG']['FMD']['botdetection1']['1']         = 'ボット検出のimportメソッドを使用したデモ1。';
+$GLOBALS['TL_LANG']['FMD']['botdetection2']['0']         = 'ボット検出のデモ2';
+$GLOBALS['TL_LANG']['FMD']['botdetection2']['1']         = 'ボット検出のデモ2(フォーム)。';
+$GLOBALS['TL_LANG']['FMD']['browscapcachegenerate']['0'] = '管理者専用: Btowscapのキャッシュ生成';
+$GLOBALS['TL_LANG']['FMD']['browscapcachegenerate']['1'] = '管理者専用: Browscapのキャッシュ生成、90秒までの実行時間';
+$GLOBALS['TL_LANG']['MOD']['botdetection']['0']          = 'ボット検出';
+$GLOBALS['TL_LANG']['MOD']['botdetection']['1']          = '検索エンジン、ボット、スパイダー、クローラーを検出するヘルパークラスです。';
 
